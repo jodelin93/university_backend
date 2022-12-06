b450ac2a39bd1ba6fe60e846a951e2a0aca5c720
Jodelin Desramaeaux <djodelin93@gmail.com>
2022-12-05 19:14:48 -0500

    finalize module employee

--- dist/src/employees/entities/employee.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Person } from 'src/persons/entities/person.entity';
-export declare class Employee {
-    id: number;
-    fonction: string;
-    date_embauche: Date;
-    personId: number;
-    person: Person;
-}
